7fd56596c008feda24b0a0d495b972dfef924e3d
Fernando Dias Motta <fernando.motta@prilltc.onmicrosoft.com>
2024-10-27 15:52:53 -0300

    Feat: Finalizando desafio, herdando classes e implementando metodos abstratos, realizando testes.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string IMEI, int memoria) : base(numero, modelo, IMEI, memoria)
+        {
+            Console.WriteLine("Criando um iPhone...");
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo: {nomeApp}...");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{nomeApp} instalado com sucesso");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var celular1 = new Nokia("1234567890", "N95", "DFSSFH233SD", 512);
+var celular2 = new Iphone("9876543210", "iPhone16", "KFDFSSFFA32S", 8);
+
+Console.WriteLine($"Teste celular: {celular1.Numero}");
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo("Whatsapp");
+Console.WriteLine("------------------------------------------------");
+
+Console.WriteLine($"Teste celular: {celular2.Numero}");
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Candy Crush");
+
+Console.ReadLine();
